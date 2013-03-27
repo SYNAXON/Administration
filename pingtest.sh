eba664ea010ef7ecbb2e88fb9787c87abc75d73e
Jörg Kastning <jkastning@jk-12.10-desktop>
2013-03-27 10:46:45 +0100

    Just finished version 1.0 and removed files not needed.

--- pingtest.sh
+++ pingtest.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Version 0.1
+# Version 1.0
 # Mit diesem Script kann die Erreichbarkeit mehrerer HOSTs via ICMP geprueft werden.
 # Die zu pruefenden Hostnamen werden zeilenweise aus einer Textdatei namens hosts.txt
 # ausgelesen. Diese Textdatei sollte moeglichst die FQDNs der zu pruefenden HOSTs enthalten.
@@ -8,7 +8,12 @@ if [ $1 == "hosts.txt" ]
   then
     while read hosts
     do
-	echo $hosts
+	echo "==================================" >>log.txt
+	date >>log.txt
+#	echo $hosts >>log.txt
+	ping -q -c 100 $hosts >>log.txt
+	date >>log.txt
+	echo "==================================" >>log.txt
     done < $1
   else
     echo "Datei hosts.txt wurde nicht gefunden."
