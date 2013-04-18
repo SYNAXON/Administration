58d76b492c9d3ba2133831ef910849095b92b754
Joerg Kastning <tronde@my-it-brain.de>
2013-04-18 12:27:46 +0300

    add error handling

--- copy_authorized_user_keys.sh
+++ copy_authorized_user_keys.sh
@@ -10,7 +10,61 @@ FTP_PASS=""
 DS_FILE="authorized_user_keys"
 LOCAL_DIR="/home/$USER/.ssh"
 
+# Functions ############################################################### #
+
+usage()
+{
+cat << EOF
+usage: $0 options
+
+This script download the authorized_user_keys file from a FTP-Server and
+copies the file to ~/.ssh of the current user.
+
+The following variables have to be specified in the script.
+FTP_HOST
+FTP_USER
+FTP_PASS
+
+You can specify them by editing the script or use the following options.
+
+OPTIONS:
+-h Show this message
+-H ftp host name
+-u ftp user name
+-p password for the ftp user
+EOF
+}
+
 # Programmstart
+while getopts .hH:u:p. OPTION
+do
+case $OPTION in
+         h)
+             usage
+             exit 1
+             ;;
+         H)
+             FTP_HOST=$OPTARG
+             ;;
+         u)
+             FTP_USER=$OPTARG
+             ;;
+         p)
+             FTP_PASS=$OPTARG
+             ;;
+         ?)
+             usage
+             exit
+             ;;
+     esac
+done
+
+if [[ -z $FTP_HOST ]] || [[ -z $FTP_USER ]] || [[ -z $FTP_PASS ]]
+then
+usage
+     exit 1
+fi
+
 mkdir -p $LOCAL_DIR
 cd $LOCAL_DIR
 
